3ccbbfc24a819e468ea5f61269e92f308f5b5d01
Oderystella <ddott59@gmail.com>
2021-06-13 18:02:18 +0900

    feat: add IndexView

--- place/views.py
+++ place/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from django.views import generic
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+class CategoryListView(generic.ListView):
+    pass
