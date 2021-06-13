4b82380c1f3d5ccd2d75598b537c2c9ee9a8fbfb
Oderystella <ddott59@gmail.com>
2021-06-13 23:48:48 +0900

    feat: add selfie when signing uo
    - add field 'selfie' in user/forms.py
    - add attribute 'enctype' in form tag for uploading image
    - enctype="multipart/form-data" is necessary for file type when method is POST

--- user/forms.py
+++ user/forms.py
@@ -6,5 +6,4 @@ class SignupForm(UserCreationForm):
 
     class Meta:
         model = get_user_model()   # settings.py에서 AUTH_USER_MODEL이 가르키는 모델을 자동으로 찾아주는 함수
-        fields = ['email', 'name'] # 정의된 모델에서 폼에 보여줄 필드를 정의함, password는 UserCreationForm에서 자동으로 생성해줌
-        
+        fields = ['email', 'name', 'selfie'] # 정의된 모델에서 폼에 보여줄 필드를 정의함, password는 UserCreationForm에서 자동으로 생성해줌
