7a4723a24fff3bea2eeb3550cf839e2ad389bb5b
Oderystella <ddott59@gmail.com>
2021-06-13 18:49:44 +0900

    feat: add url of signup
    - create SignupView in user/views.py
    - add url in user/urls.py
    - create user/templates/user/user_form.html

--- user/views.py
+++ user/views.py
@@ -8,3 +8,5 @@ from user.models import User
 class SignupView(CreateView):
     model = User                            # 자동생성 폼에서 사용할 모델, User 모델에 정의된 필드들 사용, model이 정의되면 Form 객체 자동 생성
     fields = ['email', 'name', 'password']  # 자동생성 폼에서 사용할 필드
+    # template_name_suffix = '_form'
+    # template_name = 'user_form.html'
