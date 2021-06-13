dcb8b15e5e715d0039f2079d4dda84615bce525c
Oderystella <ddott59@gmail.com>
2021-06-13 17:33:13 +0900

    feat: add signup template
    - create signup_form.html
    - create SignupView in user/views.py with CreateView
    - add url user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from user.views import SignupView
 
 app_name='user'
 
 urlpatterns = [
+    path('signup/', SignupView.as_view(), name='signup')
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
 
-# Create your views here.
+from user.models import User
+
+# CreateView로 회원가입뷰 생성하기
+# TemplateView 와 다르게 model, fields 클래스 변수 추가
+class SignupView(CreateView):
+    model = User                            # 자동생성 폼에서 사용할 모델, User 모델에 정의된 필드들 사용, model이 정의되면 Form 객체 자동 생성
+    fields = ['email', 'name', 'password']  # 자동생성 폼에서 사용할 필드
