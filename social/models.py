c61ecde16e2cc4e724b95b23efd4eda1d5bf289a
Oderystella <ddott59@gmail.com>
2021-06-14 00:52:22 +0900

    settings: create social/models.py
    - makemigrations, migrate

--- social/models.py
+++ social/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+from behaviors import BaseField
+
+class Relationship(BaseField):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='relationship')
+    followers = models.ManyToManyField(User, blank=True, related_name='following')
