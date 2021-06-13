17fcb05e26c2b021a8f7be18c3a4e233aa2371b1
Oderystella <ddott59@gmail.com>
2021-06-14 00:48:55 +0900

    settings: create model place/models.py
    - makemigrations, migrate

--- place/models.py
+++ place/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+from behaviors import BaseField
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Place(BaseField):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='place')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='place')
+    name = models.CharField(max_length=64)
+    location = models.CharField(max_length=64)
+    memo = models.TextField()
+    best_menu = models.CharField(max_length=64)
+    additional_info = models.CharField(max_length=200)
+    stars = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='place')
+    like_comments = models.ManyToManyField(User, blank=True, related_name='like_place')
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, blank=True, related_name='photo')
+    image = models.ImageField(upload_to='images/', default='default.png', blank=True, null=True)
+
+class Comment(BaseField):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='comment')
+    commenter = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='comment')
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+
+
