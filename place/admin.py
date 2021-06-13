17131a5a75d4ed932e83649eac13023e615e7952
Oderystella <ddott59@gmail.com>
2021-06-14 01:19:11 +0900

    settings: register models in place/admin.py

--- place/admin.py
+++ place/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from place.models import Category, Place, Photo, Tag, Comment
+
+admin.site.register(Category)
+admin.site.register(Place)
+admin.site.register(Photo)
+admin.site.register(Tag)
+admin.site.register(Comment)
