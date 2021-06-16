5bdf0fa1f63e2c806ebd494c36a32da1d02fb2ea
Oderystella <ddott59@gmail.com>
2021-06-17 02:24:51 +0900

    feat: add verification_email url, UserVerificationView
    - add verification_email url with pk, token in user/urls.py
    - create verification.html for html_message in
    user/templates/user/verification.html
    - create UserVericationView in user/views.py
    - 6. 가입한 메일로 인증 메일이 왔는지 확인
    - 7. 인증 메일 url을 클릭하여 인증하기, url에 user.pk와 token으로 check_token함
    - 8-1. 정상적인 사용자의 경우 is_active를 True로 바꿔주고 인증에 성공했다는 메시지 띄우고 login페이지로 redirect
    - 8-2. 비정상적인 사용자의 경우 인증에 실패했다는 메시지 띄우고, login페이지로 redirect

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from user.views import UserSignupView, UserLoginView
+from user.views import UserSignupView, UserLoginView, UserVerificationView
 
 app_name='user'
 
 urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='signup'),
+    path('<pk>/verify/<token>/', UserVerificationView.as_view(), name='verify'),
     path('login/', UserLoginView.as_view(), name='login'),
 ]
