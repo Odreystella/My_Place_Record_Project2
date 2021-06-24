9b68acc65f7bdbd72bf3ac6b0d4cac1daf7a3c4f
Oderystella <ddott59@gmail.com>
2021-06-24 17:24:16 +0900

    settings: add LOGIN_URL

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,7 @@ MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+LOGIN_URL = 'user/login'
 
 LOGIN_REDIRECT_URL = '/'  # 로그인 되면 '/'로 이동
 
